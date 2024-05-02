705e159f8c4191b630834bb56d56741f20d756df
Zeinab Ali <133343155+ZeinabAli22@users.noreply.github.com>
2024-05-02 19:23:20 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:proj_app/pages/jump.dart';
 import 'package:proj_app/pages/medical_history.dart';
 import 'package:proj_app/pages/storie_screen.dart';
 import 'firebase_options.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:proj_app/layout/home_layout.dart';
 import 'package:proj_app/pages/Instruction.dart';
 // import 'package:proj_app/pages/Jump.dart';
@@ -21,16 +21,16 @@ import 'package:proj_app/pages/homescreenboy.dart';
 import 'package:proj_app/pages/homescreengirl.dart';
 import 'package:proj_app/pages/signuup.dart';
 
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-//   runApp(const MyApp());
-// }
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+// void main() {
+//   runApp(const MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -42,8 +42,8 @@ class MyApp extends StatelessWidget {
       // home: Auth(),
       // initialRoute: "/",
       routes: {
-        // '/': (context) => const Auth(),
-        "/": (context) => const Jump(),
+        '/': (context) => const Auth(),
+        // "/": (context) => const Jump(),
         "sign": (context) => const Sign(),
         "signup": (context) => const Signup(),
         "signuup": (context) => const SignUp(),
